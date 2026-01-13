bf45c1b4d17d32d5bedf331a6ed4e758995d6d4d
Tnuji <oso2849@mavs.uta.edu>
2026-01-12 20:16:40 -0600

    destroy and numvertices

--- list.c
+++ list.c
@@ -6,7 +6,7 @@ typedef struct graph* graphPT;
 typedef struct node* nodePT;
 
 /* Graph structure (adjacency list representation) */
-typedef struct
+typedef struct graph
 {
     int undirected;   // 1 = undirected, else is directed
     int N;            // number of vertices
@@ -54,11 +54,51 @@ nodePT delete_by_data(nodePT L, int data)
 }
 
 /* Graph creation / destruction */
-graphPT newGraph(int N, int undirected);
-void destroyGraph(graphPT g);
+graphPT newGraph(int N, int undirected)
+{
+    graphPT result = malloc (sizeof(*result));
+    result -> undirected = undirected;
+    result -> E = malloc(N * sizeof(nodePT));
+
+    for(int i = 0; i < N; ++i)
+    {
+        result -> E[i] = NULL;
+    }
+    
+    return result;
+}
+void destroy_list(nodePT L)
+{
+    if(L == NULL) return;
+
+    nodePT curr = L;
+
+    while(curr != NULL)
+    {
+        nodePT next = curr -> next;
+        free(curr);
+        curr = next;
+    }
+}
+void destroyGraph(graphPT g)
+{
+    if (g == NULL) return;
+
+    for(int i = 0; i < g -> N; ++i)
+    {
+        destroy_list(g -> E[i]);
+    }
+
+    free(g -> E);
+    free(g);
+}
 
 /* Graph properties */
-int numVertices(graphPT g);
+int numVertices(graphPT g)
+{
+    if(g == NULL)return;
+    return g->N;
+}
 
 /* Vertex operations */
 int* vertexNeighbors(graphPT g, int v, int* res_size);
